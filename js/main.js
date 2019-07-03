a847e9e0a3bc847d361529739662d468dd5633c1
Nikolay Barsukov <nb721@mail.ru>
2019-07-03 17:47:33 +0700

    Добавил рендеринг профайла

--- js/main.js
+++ js/main.js
@@ -1,72 +1,22 @@
-// Пример получения и вывода списка книг
-const renderBook = book => `
-    <div class="book">
-        <div class="book_name">${book.name}</div>
-        <div class="book_author">${book.author}</div>
-    </div>
-`;
-
-const getAllBooks = function() {
-  createRequest({ path: "/api/v001/books", method: "GET" })
-    .then(response => {
-      document.querySelector("#books").innerHTML = response
-        .map(renderBook)
-        .join("");
-      console.log("Результат запроса книг", response);
-    })
-    .catch(err => {
-      document.querySelector("#books").innerHTML =
-        "Не удалось получить список книг";
-      console.log("Ошибка при получении списка книг", err);
-    });
+const renderUserPage = function () {
+    let userId = document.querySelector('.select_control-user').value;
+    createRequest({path: `api/v009/users/${userId}`, method: "Get"})
+        .then(response =>{
+            document.querySelector("#userName").innerHTML = response.login;
+            document.querySelector("#Karma").innerHTML = response.karma;
+            document.querySelector("#profileStatus").innerHTML = response.socialStatus === 1? "Общественный":"Общий";
+            console.log("Результат запроса юзера", response);
+        })
+        .catch(err=>{
+            document.querySelector("#profileBar").innerHTML = "Не удалось получить данные пользователя";
+            console.log("Ошибка", err);
+        })
 };
 
-getAllBooks();
-
-const getOneBookForm = document.querySelector("#get-one-book");
-getOneBookForm.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const data = getFieldData(event.target);
-
-  toggleClass(".one-book", "loading");
-
-  createRequest({ path: `/api/v001/books/${data.bookId}`, method: "GET" })
-    .then(response => {
-      document.querySelector("#one-book").innerHTML = renderBook(response);
-      toggleClass(".one-book", "loading");
-      console.log("Данные книги получены", response);
-    })
-    .catch(() => {
-      document.querySelector("#one-book").innerHTML =
-        "Книги с таким id не нашлось :(";
-      toggleClass(".one-book", "loading");
-      console.log("Не нашли книгу с id=", data.bookId);
-    });
-});
-
-const addBookForm = document.querySelector("#add-book");
-addBookForm.addEventListener("submit", event => {
-  event.preventDefault();
-
-  const data = getFieldData(event.target);
-  console.log("main", "data", data);
-
-  toggleClass(".add-book", "loading");
-
-  createRequest({ path: `/api/v001/books`, method: "POST" }, {}, data)
-    .then(response => {
-      toggleClass(".add-book", "loading");
-      console.log("Книга добавлена", response);
-    })
-    .catch(() => {
-      toggleClass(".add-book", "loading");
-      console.log("Не удалось добавить книгу");
-    });
-});
+renderUserPage();
 
 const userSelector = document.querySelector('.select_control-user');
 userSelector.addEventListener('change', event => {
-  userId = event.target.value;
-  getAllBooks();
+    userId = event.target.value;
+    renderUserPage();
 });
